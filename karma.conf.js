5359b7f7516034c10e3c86121bf6237c1bef41ed
DaeHyun Kim <hatemogi@gmail.com>
2014-05-08 14:23:24 +0900

    클라이언트 측 테스트 코드 디렉토리 이동

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'public/javascripts/*.js',
-      'spec/**/*.js'
+      'public/spec/*.js'
     ],
 
 
